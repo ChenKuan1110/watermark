bb2ec6f7f50c0f6cb9b3883b59bcc3e5fed5a31a
陈 宽 <18181347287@163.com>
2023-02-09 09:28:15 +0800

    feat: 阻止页面打开调试

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+window.addEventListener('keydown', (e: KeyboardEvent) => {
+  console.log(e.key, e.code)
+  if ((e.key === 'i' && e.metaKey && e.shiftKey) || e.key === 'F12') {
+    e.preventDefault()
+  }
+})
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
